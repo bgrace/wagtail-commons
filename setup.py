b74b22a44e946feb87aff4f2f935c352fa86b2ae
Brett Grace <bgrace@mac.com>
2014-06-17 10:01:08 -0700

    Convert README to rst, add setup machinery

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name='wagtail-commons',
+    version='0.0.1',
+    author=u'Brett Grace',
+    author_email='brett@codigious.com',
+    packages=['wagtail_commons'],
+    url='http://github.com/bgrace/wagtail-commons',
+    license='BSD licence, see LICENCE file',
+    description='Utility commands and mixins for Wagtail CMS',
+    long_description=open('README').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    install_requires=[
+        'pyyaml >= 3.11',
+        'markdown >= 2.4.1',
+    ]
+)
