dc3d86710fed949ca400a11fb4a2fa7f9bb430ac
Brett Grace <bgrace@mac.com>
2014-09-15 12:28:47 -0700

    Bootstrap field relations and standalone models

---
+++ wagtail_commons/core/apps.py
@@ -0,0 +1,8 @@
+__author__ = 'bgrace'
+
+from django.apps import AppConfig
+
+class WagtailCommonsCoreConfig(AppConfig):
+    label = 'wagtail_commons_bar'
+    name = 'wagtail_commons.core'
+    verbose_name = "Wagtail Commons Core"
